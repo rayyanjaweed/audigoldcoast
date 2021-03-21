5d47aec9020524d9b5d88fb9df1639db47e287f6
Rayyan Jaweed <rayyanjaweed@gmail.com>
2021-03-20 23:37:50 -0400

    Adding constructor without carId
    Also adding formatting and indentation

--- src/main/java/com/rays/autogallery/audigoldcoast/model/Car.java
+++ src/main/java/com/rays/autogallery/audigoldcoast/model/Car.java
@@ -12,6 +12,15 @@ public class Car {
 	public Car() {
 		
 	}
+	
+	public Car(long mileage, String name, String make, String model, String type) {
+		this.mileage = mileage;
+		this.name = name;
+		this.make = make;
+		this.model = model;
+		this.type = type;
+	}
+	
 	public Car(long carId, long mileage, String name, String make, String model, String type) {
 		this.carId = carId;
 		this.mileage = mileage;
@@ -20,39 +29,51 @@ public class Car {
 		this.model = model;
 		this.type = type;
 	}
+	
 	public long getCarId() {
 		return carId;
 	}
+	
 	public void setCarId(long carId) {
 		this.carId = carId;
 	}
+	
 	public long getMileage() {
 		return mileage;
 	}
+	
 	public void setMileage(long mileage) {
 		this.mileage = mileage;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getMake() {
 		return make;
 	}
+	
 	public void setMake(String make) {
 		this.make = make;
 	}
+	
 	public String getModel() {
 		return model;
 	}
+	
 	public void setModel(String model) {
 		this.model = model;
 	}
+	
 	public String getType() {
 		return type;
 	}
+	
 	public void setType(String type) {
 		this.type = type;
 	}
