8d5299e93718f2bf5c134b95c87e640a38fddc05
Rayyan Jaweed <rayyanjaweed@gmail.com>
2021-03-27 01:17:14 -0400

    Allowing Crossorigins

--- src/main/java/com/rays/autogallery/audigoldcoast/controller/CarController.java
+++ src/main/java/com/rays/autogallery/audigoldcoast/controller/CarController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +33,7 @@ public class CarController {
 		return carService.getAllCars();
 	}
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@PostMapping(path="/cars")
 	@ResponseStatus(HttpStatus.CREATED)
 	public long addCar(@RequestBody Car car) {
