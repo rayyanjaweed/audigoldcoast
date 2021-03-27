eea850ffb72d74c4fff17323c8ad9284b02f9565
Rayyan Jaweed <rayyanjaweed@gmail.com>
2021-03-27 02:21:42 -0400

    Allowing Crossorigins

--- src/main/java/com/rays/autogallery/audigoldcoast/controller/UserController.java
+++ src/main/java/com/rays/autogallery/audigoldcoast/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rays.autogallery.audigoldcoast.model.User;
 import com.rays.autogallery.audigoldcoast.service.UserService;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class UserController {
 	
